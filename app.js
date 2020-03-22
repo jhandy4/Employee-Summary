baaf90736e219656499904210e426fa8cbabe453
jhandy4 <58635628+jhandy4@users.noreply.github.com>
2020-03-22 15:56:29 -0400

    fix

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+const render = require("./lib/htmlRenderer");
 
 function promptUser() {
     return inquirer.prompt([
@@ -42,11 +47,7 @@ function promptUser() {
 
     ]);
 }
-  
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
